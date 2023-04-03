b4a2d2a15c1d838028652cb28a4644ffbeccea53
FailCake <edunad@users.noreply.github.com>
2023-04-03 17:49:12 +0200

    Change to AUTO

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -36,22 +36,32 @@
 // Compiled shaders
 #include <generated/shaders/bgfx-test/all.h>
 
-#define BGFX_EMBEDDED_SHADER_VULKAN(_name)                                                 \
-	{                                                                                      \
-		#_name,                                                                            \
-		{                                                                                  \
-			BGFX_EMBEDDED_SHADER_SPIRV(bgfx::RendererType::Vulkan,     _name)              \
-			{ bgfx::RendererType::Noop,  (const uint8_t*)"VSH\x5\x0\x0\x0\x0\x0\x0", 10 }, \
-			{ bgfx::RendererType::Count, NULL, 0 }                                         \
-		}                                                                                  \
-	}
+#if _WIN32
+	static const bgfx::EmbeddedShader s_embeddedShaders[] =
+	{
+		BGFX_EMBEDDED_SHADER(vs_cubes),
+		BGFX_EMBEDDED_SHADER(fs_cubes),
+		BGFX_EMBEDDED_SHADER_END()
+	};
+#else
+	#define BGFX_EMBEDDED_SHADER_VULKAN(_name)                                                 \
+		{                                                                                      \
+			#_name,                                                                            \
+			{                                                                                  \
+				BGFX_EMBEDDED_SHADER_SPIRV(bgfx::RendererType::Vulkan,     _name)              \
+				{ bgfx::RendererType::Noop,  (const uint8_t*)"VSH\x5\x0\x0\x0\x0\x0\x0", 10 }, \
+				{ bgfx::RendererType::Count, NULL, 0 }                                         \
+			}                                                                                  \
+		}
 
-static const bgfx::EmbeddedShader s_embeddedShaders[] =
-{
-	BGFX_EMBEDDED_SHADER_VULKAN(vs_cubes),
-	BGFX_EMBEDDED_SHADER_VULKAN(fs_cubes),
-	BGFX_EMBEDDED_SHADER_END()
-};
+	static const bgfx::EmbeddedShader s_embeddedShaders[] =
+	{
+		BGFX_EMBEDDED_SHADER_VULKAN(vs_cubes),
+		BGFX_EMBEDDED_SHADER_VULKAN(fs_cubes),
+		BGFX_EMBEDDED_SHADER_END()
+	};
+#endif
+
 
 struct PosColorVertex
 {
@@ -94,28 +104,13 @@ static void glfw_errorCallback(int error, const char *description) {
 }
 
 static void* glfwNativeWindowHandle(GLFWwindow* _window) {
-#	if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
-# 		if USE_WAYLAND
-		wl_egl_window *win_impl = (wl_egl_window*)glfwGetWindowUserPointer(_window);
-		if(!win_impl)
-		{
-			int width, height;
-			glfwGetWindowSize(_window, &width, &height);
-			struct wl_surface* surface = (struct wl_surface*)glfwGetWaylandWindow(_window);
-			if(!surface)
-				return nullptr;
-			win_impl = wl_egl_window_create(surface, width, height);
-			glfwSetWindowUserPointer(_window, (void*)(uintptr_t)win_impl);
-		}
-		return (void*)(uintptr_t)win_impl;
-#		else
-		return (void*)(uintptr_t)glfwGetX11Window(_window);
-#		endif
-#	elif BX_PLATFORM_OSX
-		return glfwGetCocoaWindow(_window);
-#	elif BX_PLATFORM_WINDOWS
-		return glfwGetWin32Window(_window);
-#	endif // BX_PLATFORM_
+	#if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
+			return (void*)(uintptr_t)glfwGetX11Window(_window);
+	#elif BX_PLATFORM_OSX
+			return glfwGetCocoaWindow(_window);
+	#elif BX_PLATFORM_WINDOWS
+			return glfwGetWin32Window(_window);
+	#endif
 }
 
 int main(int argc, char* argv[]) {
@@ -134,11 +129,8 @@ int main(int argc, char* argv[]) {
 	GLFWwindow *window = glfwCreateWindow(width, height, "test", nullptr, nullptr);
 	if (!window) return 1;
 
-	// Call bgfx::renderFrame before bgfx::init to signal to bgfx not to create a render thread.
-	// Most graphics APIs must be used on the same thread that created the window.
-	bgfx::renderFrame();
 	bgfx::Init init;
-	init.type = bgfx::RendererType::Vulkan;
+	init.type = bgfx::RendererType::Count;
 	init.resolution.width = (uint32_t)width;
 	init.resolution.height = (uint32_t)height;
 	init.resolution.reset = BGFX_RESET_VSYNC;
