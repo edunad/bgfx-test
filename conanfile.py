de2f2ae19a273193db5f440f2d1e467169021ad3
FailCake <edunad@users.noreply.github.com>
2023-04-04 11:40:31 +0200

    Fix wayland

---
+++ conanfile.py
@@ -0,0 +1,11 @@
+from conan import ConanFile
+
+class Recipe(ConanFile):
+	settings = "os", "compiler", "build_type", "arch"
+	requires = "fmt/9.1.0"
+	generators = "CMakeDeps", "CMakeToolchain"
+
+	def requirements(self):
+		if self.settings.os == "Linux":
+			self.requires("wayland/1.21.0")
+
